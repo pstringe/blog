1aa86d3e4c2a6c4f0b0a3583d0c2673ec44c0687
Poitier Stringer <stringerpoitier@gmail.com>
2021-03-22 10:15:57 -0700

    added error message on home component

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import PostList from './PostList';
 const Home = () => {
     const [posts, setPosts] = useState([null]);
     const [isPending, setIsPending] = useState(true);
-
+    const [error, setError] = useState(false);
     useEffect(() => {
         fetch('http://localhost:8000/posts')
         .then(res => {
@@ -14,17 +14,21 @@ const Home = () => {
             return res.json()
         })
         .then((data) => {
+            setError(false);
             setPosts(data);
             setIsPending(false);
         })
         .catch((err) => {
             console.log(err.message);
+            setIsPending(false);
+            setError(true);
         })
     }, []);
 
     return ( 
         <div className='home'>
             <h1>Home</h1>
+            {error && <div>There was an error...</div>}
             {isPending && <div className='loading'>Loading...</div>}
             {posts && <PostList posts={posts} heading='Featured'/>}
         </div>
