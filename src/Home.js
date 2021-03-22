4bc04f4e62a7d6c20623bce893c66a992fde8e52
Poitier Stringer <stringerpoitier@gmail.com>
2021-03-22 10:09:19 -0700

    added error handeling for data retrieval

--- src/Home.js
+++ src/Home.js
@@ -3,22 +3,29 @@ import PostList from './PostList';
 
 const Home = () => {
     const [posts, setPosts] = useState([null]);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
         fetch('http://localhost:8000/posts')
         .then(res => {
+            if (!res.ok){
+                throw Error('response from server is not ok');
+            }
             return res.json()
         })
         .then((data) => {
             setPosts(data);
-        }
-    );
-
-    },);
+            setIsPending(false);
+        })
+        .catch((err) => {
+            console.log(err.message);
+        })
+    }, []);
 
     return ( 
         <div className='home'>
             <h1>Home</h1>
+            {isPending && <div className='loading'>Loading...</div>}
             {posts && <PostList posts={posts} heading='Featured'/>}
         </div>
     );
