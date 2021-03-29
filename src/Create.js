3a8029d7c89f2d12aa7ca79377a4ad3805b723c9
Poitier Stringer <stringerpoitier@gmail.com>
2021-03-29 15:24:40 -0700

    added blog form

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,21 @@ const Create = () => {
     return (
         <div className='create'>
             <h1>Create a new Post</h1>
+            <form>
+                <label>Title</label>
+                <input type='text' required></input>
+                <label>Body</label>
+                <textarea required>
+                    
+                </textarea>
+                <label>Author</label>
+
+                <select>
+                    <option value='Poitier'>Poitier</option>
+                    <option value='Mario'>Mario</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
     );
 }
