67c1b707401f1a6bb463bdb19be4965a3bd23fc0
Poitier Stringer <stringerpoitier@gmail.com>
2021-04-04 12:42:20 -0700

    added typography to post details

--- src/PostList.js
+++ src/PostList.js
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom';
+import {Typography} from '@material-ui/core';
 
 const PostList = ({heading, posts}) => {
     return posts ? ( 
@@ -8,13 +9,13 @@ const PostList = ({heading, posts}) => {
                 return post ? (
                 <div className='post-preview' key={post.id}>
                     <Link to={`/posts/${post.id}`}>
-                        <h2>{post?.title}</h2>
+                        <Typography component='h2' variant='h6' color='textSecondary'>{post?.title}</Typography>
                         <div className='post-preview-author'>
-                            <p>{post?.author}</p>
+                            <Typography component='p' variant='subtitle2' gutterBottom>{post?.author}</Typography>
                         </div>
                     </Link>
                     <div className='post-preview-body'>
-                        <p>{post?.body}</p>
+                        <Typography component='p' variant='body1' paragraph>{post?.body}</Typography>
                     </div>
                 </div>
                 ): null;
