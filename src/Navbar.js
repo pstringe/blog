4b5c32bafa07363ec6569fdad70e814efe4f2ed1
Poitier Stringer <stringerpoitier@gmail.com>
2021-04-04 15:30:09 -0700

    added icon to create menu item

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
 import {Typography, Button,  ButtonGroup} from '@material-ui/core';
-
+import AddCircleOutlineRoundedIcon from '@material-ui/icons/AddCircleOutlineRounded';
 const Navbar = (props) => {
     return ( 
         <nav className='navbar'>
             <Typography color='primary' variant='h5' component='h1'> {props.title} </Typography>
             <ButtonGroup variant='text' color='secondary' className="links">
                 <Link to='/'><Button>Home</Button></Link>
-                <Link to='/create'><Button>Create</Button></Link>
+                <Link to='/create'><Button endIcon={<AddCircleOutlineRoundedIcon/>}>Create</Button></Link>
             </ButtonGroup>
         </nav>
     );
