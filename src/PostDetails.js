8f7298658f9ca0010de0e90cb06704be75331909
Poitier Stringer <stringerpoitier@gmail.com>
2021-04-04 12:49:49 -0700

    adjusted layout for post details

--- src/PostDetails.js
+++ src/PostDetails.js
@@ -6,15 +6,14 @@ const PostDetails = () => {
     const {data : post, error, isPending } = useFetch('http://localhost:8000/posts/' + id);
     return (
        <div className='post-details'>
-           <h2>post details{id}</h2>
            {isPending && <div>Loading...</div>}
            {error && <div>{error}</div>}
            {(!isPending && !error && 
-            <div className='post'>
+            <>
                 <h2>{post.title}</h2>
                 <p className='post-author'>{post.author}</p>
                 <p className='post-body'>{post.body}</p>
-            </div>
+            </>
            )}
        </div> 
     );
