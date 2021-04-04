0b94d3774e2877d2e4e7abefe85ed935bbacb7d1
Poitier Stringer <stringerpoitier@gmail.com>
2021-04-04 13:38:35 -0700

    added button-group to nav

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
-import {Typography} from '@material-ui/core';
+import {Typography, Button,  ButtonGroup} from '@material-ui/core';
 
 const Navbar = (props) => {
     return ( 
         <nav className='navbar'>
             <Typography color='primary' variant='h5' component='h1'> {props.title} </Typography>
-            <div className="links">
-                <Link to='/'><Typography color='secondary'>Home</Typography></Link>
-                <Link to='/create'><Typography color='secondary'>Create</Typography></Link>
-            </div>
+            <ButtonGroup variant='text' color='secondary' className="links">
+                <Link to='/'><Button>Home</Button></Link>
+                <Link to='/create'><Button>Create</Button></Link>
+            </ButtonGroup>
         </nav>
     );
 }
